9c5c3e71f4ebc070e34ffd5072e2d2c954f5d29c
BillChan226 <billchenzr226@outlook.com>
2023-06-14 10:30:03 +0800

    surface segmentation

--- utility/region_growing.py
+++ utility/region_growing.py
@@ -16,8 +16,8 @@ edges1 = np.array(e1)
 
 print("vertices1", np.shape(vertices1))
 
-pcd1 = o3d.geometry.PointCloud()
-pcd1.points = o3d.utility.Vector3dVector(vertices1)
+# pcd1 = o3d.geometry.PointCloud()
+# pcd1.points = o3d.utility.Vector3dVector(vertices1)
 
 # o3d.visualization.draw_geometries([pcd1])
 
@@ -41,19 +41,19 @@ p.from_array(vertices1)
 
 vg = p.make_voxel_grid_filter()
 # vg.set_leaf_size(0.01, 0.01, 0.01)
-vg.set_leaf_size(0.1, 0.1, 0.1)
+vg.set_leaf_size(0.5, 0.5, 0.5)
 
 cloud_filtered = vg.filter()
 tree = cloud_filtered.make_kdtree()
-segment = cloud_filtered.make_RegionGrowing(ksearch=50)
-segment.set_MinClusterSize(100)
+segment = cloud_filtered.make_RegionGrowing(ksearch=30)
+segment.set_MinClusterSize(200)
 segment.set_MaxClusterSize(25000)
 segment.set_NumberOfNeighbours(5)
 
 #https://legacy.gitbook.com/book/adioshun/pcl/edit#
-segment.set_SmoothnessThreshold(0.2)
+segment.set_SmoothnessThreshold(0.1)
 # segment.set_CurvatureThreshold(0.05)
-segment.set_CurvatureThreshold(0.02)
+segment.set_CurvatureThreshold(0.01)
 
 segment.set_SearchMethod(tree)
 cluster_indices = segment.Extract()
@@ -63,12 +63,40 @@ print("cluster_indices", np.shape(cluster_indices))
 for i in cluster_indices:
     print(np.shape(i)) 
     
-print("cluster_indices[0]", cluster_indices[0])
+# print("cluster_indices[0]", cluster_indices[0])
     
 cloud_cluster = pcl.PointCloud()
 
+# cloud_cluster = pcl.PointCloud()
+all_points = []
+for j, indices in enumerate(cluster_indices):
+    # print('indices = ' + str(len(indices)))
+    points = np.zeros((len(indices), 3), dtype=np.float32)
+
+    for i, indice in enumerate(indices):
+        points[i][0] = cloud_filtered[indice][0]
+        points[i][1] = cloud_filtered[indice][1]
+        points[i][2] = cloud_filtered[indice][2]
+
+    cloud_cluster.from_array(points)
+    
+    all_points.append(points)
+
 # visual = pcl.pcl_visualization.CloudViewing()
 
+print("points", np.shape(all_points))
+
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(all_points[0])
+# pcd.points = o3d.utility.Vector3dVector(vertices1[cluster_indices[0]])
+
+o3d.visualization.draw_geometries([pcd])
+
+# # Create an array of random colors for each point in the point cloud
+# color = [0.5, 0.5, 0.5]
+
+# # Assign the colors to the point cloud
+# pcd.colors = o3d.utility.Vector3dVector(color)
 
 
 # # PointXYZ
